773fc9f605660d1832edaf4b0f817f454f158d7d
Niyanta Lad <niyantalad0604@gmail.com>
2022-09-20 15:59:34 -0400

    Time: 51 ms, Memory: 14.2 MB - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,17 +1,13 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         hashmap={}
         for i,j in enumerate(nums):
             if target-nums[i] in hashmap:
                 return ([hashmap[target-j],i])
-            hashmap[j]=i                                #O(n)
-            
-
-#         for m in range (0,len(nums)):
-#             for n in range (m+1,len(nums)):               
-#                 if nums[m]+nums[n]==target:
-#                     return([m,n])                     #O(n**2)
-                                          
-
-        
+            hashmap[j]=i  
         
