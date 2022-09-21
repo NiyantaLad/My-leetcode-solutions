fcf8ccd0b538d6a974a850933c60813ae5b673c1
Niyanta Lad <niyantalad0604@gmail.com>
2022-09-20 23:58:34 -0400

    Time: 41 ms, Memory: 13.5 MB - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -8,7 +8,13 @@ class Solution(object):
         l=[]
         dict1={}
         for i in nums1:
-            dict1[i]=nums1.count(i)
+            # if i not in dict1:
+            # dict1[i]=nums1.count(i)
+                if i not in dict1:
+                    dict1[i]=1
+                else:
+                    dict1[i]+=1
+
         for i in nums2:
             if i in dict1 and dict1[i]>=1:
                 dict1[i]-=1
