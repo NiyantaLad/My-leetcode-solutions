f0c4d8f3d1355273516d5fb2913ef04eff5ab92a
Niyanta Lad <niyantalad0604@gmail.com>
2023-03-25 15:48:12 -0400

    Time: 0 ms, Memory: 42.3 MB - LeetHub

--- merge-sorted-array/merge-sorted-array.java
+++ merge-sorted-array/merge-sorted-array.java
@@ -1,24 +1,31 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i,j;
-        if(m>0 && n>0){
-            for ( i=0,j=0;i<nums1.length;i++){
-                if (nums1[i]==0 && n!=0){
-                    nums1[i]=nums2[j];
-                    j++;
-                    n--;
-                }
-            }
-            Arrays.sort(nums1);
+        
+        int i= m-1;
+        int j= n-1;
+        int k= m+n-1;
+        while(j>=0 && i>=0){
+            if (nums1[i]<=nums2[j])
+                nums1[k--]=nums2[j--];
+            else
+                nums1[k--]=nums1[i--];
         }
-        if (m==0 && n>0){
-             i=0;
-             j=0;
-            while(i!=n){
-                nums1[i]=nums2[i];
-                i++;
-                j++;
-            }
-        }
+        
+        while(j>=0)  
+            nums1[k--]=nums2[j--];
+        
+        
+        
+        
+        
+        
+
+        //Approach 2: using Arrays.sort()
+       //  for (int i = m; i<m+n; i++){
+       //     nums1[i] = nums2[i-m];   
+       // }
+       //  Arrays.sort(nums1);
+    //  Time Complexity = O(n log(n)) for Arrays.sort method
+    //  Space Complexity = O(1)
     }
 }
