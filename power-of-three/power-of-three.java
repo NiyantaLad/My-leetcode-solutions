f889f60ed0d1cf7acbe66218840dbb11c4f97c0f
Niyanta Lad <niyantalad0604@gmail.com>
2023-03-26 21:09:49 -0400

    Time: 16 ms, Memory: 42.5 MB - LeetHub

---
+++ power-of-three/power-of-three.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        
+        if(n<0)
+            return false;
+        
+        if ((Math.log10(n)/Math.log10(3)) %1==0)
+            return true;
+        else 
+            return false;
+        
+    }
+}
