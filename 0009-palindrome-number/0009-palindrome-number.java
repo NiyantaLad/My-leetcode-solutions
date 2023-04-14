90e36d925b4bd7aae8610138f001332859f5e329
Niyanta Lad <niyantalad0604@gmail.com>
2023-04-13 21:53:23 -0400

    Time: 9 ms (99.11%), Space: 41.9 MB (72.81%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,18 +1,36 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = Integer.toString(x);
-        char[] xarr = s.toCharArray() ;
         
-        int i=0;
-        int j=xarr.length-1;
+        if(x < 0) 
+            return false; 
         
-        while(i<j){
-            if(xarr[i]!=xarr[j])
-                return false;
-            i++;
-            j--;
+        
+        //Approach 1: convert to string
+//         String s = Integer.toString(x);
+//         char[] xarr = s.toCharArray() ;
+        
+//         int i=0;
+//         int j=xarr.length-1;
+        
+//         while(i<j){
+//             if(xarr[i]!=xarr[j])
+//                 return false;
+//             i++;
+//             j--;
+//         }
+        
+//         return true;
+        
+        
+        //Approach 2: without converting to string
+        
+        int reversed=0, remainder, original=x;
+        while(x!=0){
+            remainder = x%10;
+            reversed = reversed*10 + remainder;
+            x=x/10;
         }
+        return original==reversed;
         
-        return true;
     }
 }
