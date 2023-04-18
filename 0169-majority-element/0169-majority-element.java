5d036e9720a26145320401b3e168bc761e411daa
Niyanta Lad <niyantalad0604@gmail.com>
2023-04-18 18:05:00 -0400

    Time: 15 ms (22.50%), Space: 48.1 MB (13.16%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,50 @@
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         HashMap <Integer, Integer> map = new HashMap<>();
+//         int majority=0;
+//         int maxCount=0;
+//         if (nums.length==1)
+//             return nums[0];
+//         for (int i=0; i<nums.length;i++){
+//             if (map.containsKey(nums[i])){
+//                 map.put(nums[i], (map.get(nums[i])) + 1);                
+//                 if( (map.get(nums[i])>maxCount)){
+//                     majority=nums[i];
+//                     maxCount=map.get(nums[i]);
+//                 }                                
+//             }                                      
+//             else {
+//                 map.put(nums[i],1);
+//             }
+//         }
+//         return majority;
+//     }                                      
+// }
+
 class Solution {
-    public int majorityElement(int[] nums) {
-        HashMap <Integer, Integer> map = new HashMap<>();
-        int majority=0;
-        int maxCount=0;
-        if (nums.length==1)
-            return nums[0];
-        for (int i=0; i<nums.length;i++){
-            if (map.containsKey(nums[i])){
-                map.put(nums[i], (map.get(nums[i])) + 1);                
-                if( (map.get(nums[i])>maxCount)){
-                    majority=nums[i];
-                    maxCount=map.get(nums[i]);
-                }                                
-            }                                      
+    private Map<Integer, Integer> countNums(int[] nums) {
+        Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            if (!counts.containsKey(num)) {
+                counts.put(num, 1);
+            }
             else {
-                map.put(nums[i],1);
+                counts.put(num, counts.get(num)+1);
             }
         }
-        return majority;
-    }                                      
+        return counts;
+    }
+
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> counts = countNums(nums);
+
+        Map.Entry<Integer, Integer> majorityEntry = null;
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
+                majorityEntry = entry;
+            }
+        }
+
+        return majorityEntry.getKey();
+    }
 }
