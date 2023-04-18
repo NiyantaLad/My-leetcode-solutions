5a9c6b93a44d6fb3ade5d01ab1194aa20ef738fc
Niyanta Lad <niyantalad0604@gmail.com>
2023-04-18 18:08:37 -0400

    Time: 2 ms (67.40%), Space: 45.8 MB (83.00%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,38 @@
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         HashMap <Integer, Integer> map = new HashMap<>();
+//         int majority=0;
+//         int maxCount=0;
+//         if (nums.length==1)
+//             return nums[0];
+//         for (int i=0; i<nums.length;i++){
+//             if (map.containsKey(nums[i])){
+//                 map.put(nums[i], (map.get(nums[i])) + 1);                
+//                 if( (map.get(nums[i])>maxCount)){
+//                     majority=nums[i];
+//                     maxCount=map.get(nums[i]);
+//                 }                                
+//             }                                      
+//             else {
+//                 map.put(nums[i],1);
+//             }
+//         }
+//         return majority;
+//     }                                      
+// }
+
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap <Integer, Integer> map = new HashMap<>();
-        int majority=0;
-        int maxCount=0;
-        if (nums.length==1)
-            return nums[0];
-        for (int i=0; i<nums.length;i++){
-            if (map.containsKey(nums[i])){
-                map.put(nums[i], (map.get(nums[i])) + 1);                
-                if( (map.get(nums[i])>maxCount)){
-                    majority=nums[i];
-                    maxCount=map.get(nums[i]);
-                }                                
-            }                                      
-            else {
-                map.put(nums[i],1);
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
             }
+            count += (num == candidate) ? 1 : -1;
         }
-        return majority;
-    }                                      
+
+        return candidate;
+    }
 }
