8145d582dc0cd2f7e945eab14b6a59f744adbe51
Niyanta Lad <niyantalad0604@gmail.com>
2023-04-18 18:53:50 -0400

    Time: 4 ms (33.79%), Space: 52.6 MB (70.44%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -6,10 +6,8 @@ class Solution {
             if (nums[i] > nums[i+1])
                 inc= false;
             if (nums[i] < nums[i+1])
-                dec= false;
-            
+                dec= false;            
         }
-        return inc || dec;
-        
+        return inc || dec;        
     }
 }
