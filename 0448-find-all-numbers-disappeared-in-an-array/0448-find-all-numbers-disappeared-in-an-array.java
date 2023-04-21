0913edeb3189fb0a6025a3ce58abe1f735fe2b4d
Niyanta Lad <niyantalad0604@gmail.com>
2023-04-21 15:55:45 -0400

    Time: 6 ms (83.82%), Space: 51 MB (40.08%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,6 +1,23 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
+        // without extra space and in O(n) time
         List<Integer> result = new ArrayList<>();
+        for (int i=0; i<nums.length;i++){
+            while (nums[i]!=i+1 && nums[i]!= nums[nums[i]-1]){
+                int temp=nums[i];
+                nums[i]= nums[temp-1];
+                nums[temp-1]= temp;               
+            }
+        }
+        for (int i=0; i<nums.length;i++){
+            if (nums[i]!=i+1)
+                result.add(i+1);
+        }
+        return result;
+    }
+}
+
+
         
 //         int[] count = new int[nums.length];
 //         for (int i=0; i<nums.length;i++){
@@ -13,18 +30,3 @@ class Solution {
 //         return result;
         
         
-        // without extra space and in O(n) time
-       for (int i=0; i<nums.length;i++){
-           while (nums[i]!=i+1 && nums[i]!= nums[nums[i]-1]){
-               int temp=nums[i];
-               nums[i]= nums[temp-1];
-               nums[temp-1]= temp;               
-           }
-        }
-        for (int i=0; i<nums.length;i++){
-            if (nums[i]!=i+1)
-                result.add(i+1);
-        }
-        return result;
-    }
-}
