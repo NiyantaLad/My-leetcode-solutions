ec2f4fcec2c0ee72b4f1978b99f09002c1e386a3
Niyanta Lad <niyantalad0604@gmail.com>
2023-04-21 14:40:31 -0400

    Time: 2 ms (98.42%), Space: 42.7 MB (85.29%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,21 +1,37 @@
+// class Solution {
+//     public boolean canConstruct(String ransomNote, String magazine) {
+//         if (ransomNote.length() > magazine.length()) return false;
+//         Map <Character, Integer> map = new HashMap<>();
+
+//         for (int i=0; i<magazine.length();i++){
+//             map.put(magazine.charAt(i),map.getOrDefault(magazine.charAt(i),0)+1);
+//         }
+        
+//         for (int j=0; j<ransomNote.length();j++){ 
+//             if (!map.containsKey(ransomNote.charAt(j)))
+//                 return false;
+//             else{
+//                 if (map.get(ransomNote.charAt(j))==0)
+//                     return false;
+//                 else
+//                     map.put(ransomNote.charAt(j),map.get(ransomNote.charAt(j))-1);                      
+//             }              
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        if (ransomNote.length() > magazine.length()) return false;
-        Map <Character, Integer> map = new HashMap<>();
-
-        for (int i=0; i<magazine.length();i++){
-            map.put(magazine.charAt(i),map.getOrDefault(magazine.charAt(i),0)+1);
-        }
+		if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabets_counter = new int[26];
         
-        for (int j=0; j<ransomNote.length();j++){ 
-            if (!map.containsKey(ransomNote.charAt(j)))
-                return false;
-            else{
-                if (map.get(ransomNote.charAt(j))==0)
-                    return false;
-                else
-                    map.put(ransomNote.charAt(j),map.get(ransomNote.charAt(j))-1);                      
-            }              
+        for (char c : magazine.toCharArray())
+            alphabets_counter[c-'a']++;
+
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) return false;
+            alphabets_counter[c-'a']--;
         }
         return true;
     }
