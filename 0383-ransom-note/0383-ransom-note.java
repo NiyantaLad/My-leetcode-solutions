3f6a1bb1d314865e5abba5d8b8fb3efe50d863b9
Niyanta Lad <niyantalad0604@gmail.com>
2023-04-21 14:40:58 -0400

    Time: 2 ms (98.42%), Space: 43.1 MB (37.06%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,3 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+		if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabets_counter = new int[26];
+        
+        for (char c : magazine.toCharArray())
+            alphabets_counter[c-'a']++;
+
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) return false;
+            alphabets_counter[c-'a']--;
+        }
+        return true;
+    }
+}
+
 // class Solution {
 //     public boolean canConstruct(String ransomNote, String magazine) {
 //         if (ransomNote.length() > magazine.length()) return false;
@@ -21,18 +37,4 @@
 //     }
 // }
 
-class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-		if (ransomNote.length() > magazine.length()) return false;
-        int[] alphabets_counter = new int[26];
-        
-        for (char c : magazine.toCharArray())
-            alphabets_counter[c-'a']++;
 
-        for (char c : ransomNote.toCharArray()){
-            if (alphabets_counter[c-'a'] == 0) return false;
-            alphabets_counter[c-'a']--;
-        }
-        return true;
-    }
-}
