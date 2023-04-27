364f64c3b0990a762958036dc36f3be33ad20d33
Niyanta Lad <niyantalad0604@gmail.com>
2023-04-27 15:26:15 -0400

    Time: 8 ms (53.42%), Space: 43.2 MB (62.20%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,49 +1,51 @@
-// class Solution {
-//     public String reverseWords(String s) {
-//         String[] words = s.split(" ");
-//         StringBuilder ans= new StringBuilder();
-//         for (String w: words){
-//             int i=0, j = w.length()-1;
-//             char[] word = w.toCharArray();
-//             char temp;
-//             while(i<j){
-//                 temp=word[i];
-//                 word[i]=word[j];
-//                 word[j]=temp;
-//                 i++;
-//                 j--;  
-//             }
-//             ans.append(word);
-//             ans.append(" ");            
-//         }
-
-//         // ans.setLength(ans.length() - 1);
-//         ans.deleteCharAt(ans.length()-1);
-//         return ans.toString();
-//     }
-// }
-
+//7ms
 class Solution {
-
     public String reverseWords(String s) {
-        int lastSpaceIndex = -1;
-        char[] chArray = s.toCharArray();
-        int len = s.length();
-        for (int strIndex = 0; strIndex <= len; strIndex++) {
-            if (strIndex == len || chArray[strIndex] == ' ') {
-                int startIndex = lastSpaceIndex + 1;
-                int endIndex = strIndex - 1;
-                while (startIndex < endIndex) {
-                    char temp = chArray[startIndex];
-                    chArray[startIndex] = chArray[endIndex];
-                    chArray[endIndex] = temp;
-                    startIndex++;
-                    endIndex--;
-                }
-                lastSpaceIndex = strIndex;
+        String[] words = s.split(" ");
+        StringBuilder ans= new StringBuilder();
+        for (String w: words){
+            int i=0, j = w.length()-1;
+            char[] word = w.toCharArray();
+            char temp;
+            while(i<j){
+                temp=word[i];
+                word[i]=word[j];
+                word[j]=temp;
+                i++;
+                j--;  
             }
+            ans.append(word);
+            ans.append(" ");            
         }
-        return new String(chArray);
-    }
 
+        // ans.setLength(ans.length() - 1);
+        ans.deleteCharAt(ans.length()-1);
+        return ans.toString();
+    }
 }
+
+//4ms
+// class Solution {
+
+//     public String reverseWords(String s) {
+//         int lastSpaceIndex = -1;
+//         char[] chArray = s.toCharArray();
+//         int len = s.length();
+//         for (int strIndex = 0; strIndex <= len; strIndex++) {
+//             if (strIndex == len || chArray[strIndex] == ' ') {
+//                 int startIndex = lastSpaceIndex + 1;
+//                 int endIndex = strIndex - 1;
+//                 while (startIndex < endIndex) {
+//                     char temp = chArray[startIndex];
+//                     chArray[startIndex] = chArray[endIndex];
+//                     chArray[endIndex] = temp;
+//                     startIndex++;
+//                     endIndex--;
+//                 }
+//                 lastSpaceIndex = strIndex;
+//             }
+//         }
+//         return new String(chArray);
+//     }
+
+// }
