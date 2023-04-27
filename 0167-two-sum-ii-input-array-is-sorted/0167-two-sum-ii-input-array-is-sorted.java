1e33a45fed9e0d4131889f9011ae2ae1382ad5b5
Niyanta Lad <niyantalad0604@gmail.com>
2023-04-27 19:41:16 -0400

    Time: 1 ms (98.85%), Space: 45.7 MB (14.30%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,24 +1,40 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map <Integer, Integer> map = new HashMap<>();
-        int[] output = new int[2];
-        for(int j=0; j<nums.length;j++){
-            map.put(nums[j],j);
-        }
         
-        for(int i=0;i<nums.length;i++){
-            int diff = target-nums[i];
-            if(map.containsKey(diff) && map.get(diff)!=i){
-                if((i+1)<(map.get(diff)+1)){
-                    output[0]=i+1;
-                    output[1]=map.get(diff)+1;
-                }
-                else{
-                    output[1]=i+1;
-                    output[0]=map.get(diff)+1;
-                }
-            }
+        int i=0;
+        int j=nums.length-1;
+        
+        while(i<j){
+            if(nums[i]+nums[j]==target)
+                break;
+            if(nums[i]+nums[j]<target)
+                i++;
+            else
+                j--;
         }
-        return (output);
+        return new int[] {i+1,j+1};
+        
+        
+        
+//         Map <Integer, Integer> map = new HashMap<>();
+//         int[] output = new int[2];
+//         for(int j=0; j<nums.length;j++){
+//             map.put(nums[j],j);
+//         }
+        
+//         for(int i=0;i<nums.length;i++){
+//             int diff = target-nums[i];
+//             if(map.containsKey(diff) && map.get(diff)!=i){
+//                 if((i+1)<(map.get(diff)+1)){
+//                     output[0]=i+1;
+//                     output[1]=map.get(diff)+1;
+//                 }
+//                 else{
+//                     output[1]=i+1;
+//                     output[0]=map.get(diff)+1;
+//                 }
+//             }
+//         }
+//         return (output);
     }
 }
