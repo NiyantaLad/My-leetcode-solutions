6c8163dcd83072a3cfb913df5541224195db5fe4
Niyanta Lad <niyantalad0604@gmail.com>
2023-04-27 15:00:52 -0400

    Time: 4 ms (76.75%), Space: 68.8 MB (36.11%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -10,26 +10,40 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        if(head==null || head.next==null) return null;
+        if(head.next==null) return null;
         ListNode walker = head;
-        ListNode runner = head;
-        while (runner.next!=null && runner.next.next!=null){
+        ListNode runner = head.next.next;
+        while (runner!=null && runner.next!=null){
             walker = walker.next;
             runner= runner.next.next;
-        }
-        if (runner.next==null){
-            runner = walker;
-            walker=head;
-            while (runner!=walker.next){
-                walker=walker.next;
-            }
-            walker.next=runner.next;
-            return head;
-        }
-        else if (runner.next.next==null){
-            walker.next=walker.next.next;
-            return head;
-        }
-        return null;
+        }        
+        walker.next=walker.next.next;
+        return head;        
     }
 }
+
+// class Solution {
+//     public ListNode deleteMiddle(ListNode head) {
+//         if(head==null || head.next==null) return null;
+//         ListNode walker = head;
+//         ListNode runner = head;
+//         while (runner.next!=null && runner.next.next!=null){
+//             walker = walker.next;
+//             runner= runner.next.next;
+//         }
+//         if (runner.next==null){
+//             runner = walker;
+//             walker=head;
+//             while (runner!=walker.next){
+//                 walker=walker.next;
+//             }
+//             walker.next=runner.next;
+//             return head;
+//         }
+//         else if (runner.next.next==null){
+//             walker.next=walker.next.next;
+//             return head;
+//         }
+//         return null;
+//     }
+// }
