4bbea499097985ed031eb6dba6a60570e32d499c
Niyanta Lad <niyantalad0604@gmail.com>
2023-06-17 15:20:11 -0400

    Time: 0 ms (100.00%), Space: 40.9 MB (65.72%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -3,44 +3,17 @@ class Solution {
         int m = word1.length();
         int n = word2.length();
         StringBuilder result = new StringBuilder();
+        int i = 0, j = 0;
 
-        for (int i = 0; i < Math.max(m, n); i++) {
+        while (i < m || j < n) {
             if (i < m) {
-                result.append(word1.charAt(i));
+                result.append(word1.charAt(i++));
             }
-            if (i < n) {
-                result.append(word2.charAt(i));
+            if (j < n) {
+                result.append(word2.charAt(j++));
             }
         }
 
         return result.toString();
     }
 }
-// class Solution {
-//     public String mergeAlternately(String word1, String word2) {
-//         StringBuilder sb = new StringBuilder();
-//         int i=0,len;
-//         if(word1.length()<word2.length())
-//             len=word1.length();
-//         else
-//             len=word2.length();
-//         while(i<len){
-//             sb.append(word1.charAt(i));
-//             sb.append(word2.charAt(i));
-//             i++;
-//         }
-//         if(i!=word1.length()){            
-//             while (i<word1.length()){
-//                 sb.append(word1.charAt(i));
-//                 i++;
-//             }
-//         }
-//         else if(i!=word2.length()){            
-//             while (i<word2.length()){
-//                 sb.append(word2.charAt(i));
-//                 i++;
-//             }
-//         }
-//         return sb.toString();
-//     }
-// }
