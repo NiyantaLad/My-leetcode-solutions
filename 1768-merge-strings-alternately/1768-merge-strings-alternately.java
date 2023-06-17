0f0c13627acc55d2105d5e7dd62c5e571f302501
Niyanta Lad <niyantalad0604@gmail.com>
2023-06-17 15:17:30 -0400

    Time: 1 ms (73.67%), Space: 40.7 MB (74.73%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,28 +1,47 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        StringBuilder sb = new StringBuilder();
-        int i=0,len;
-        if(word1.length()<word2.length())
-            len=word1.length();
-        else
-            len=word2.length();
-        while(i<len){
-            sb.append(word1.charAt(i));
-            sb.append(word2.charAt(i));
-            i++;
-        }
-        if(i!=word1.length()){            
-            while (i<word1.length()){
-                sb.append(word1.charAt(i));
-                i++;
+        int m = word1.length();
+        int n = word2.length();
+        StringBuilder result = new StringBuilder();
+        int i = 0, j = 0;
+
+        while (i < m || j < n) {
+            if (i < m) {
+                result.append(word1.charAt(i++));
+            }
+            if (j < n) {
+                result.append(word2.charAt(j++));
             }
         }
-        else if(i!=word2.length()){            
-            while (i<word2.length()){
-                sb.append(word2.charAt(i));
-                i++;
-            }
-        }
-        return sb.toString();
+
+        return result.toString();
     }
 }
+// class Solution {
+//     public String mergeAlternately(String word1, String word2) {
+//         StringBuilder sb = new StringBuilder();
+//         int i=0,len;
+//         if(word1.length()<word2.length())
+//             len=word1.length();
+//         else
+//             len=word2.length();
+//         while(i<len){
+//             sb.append(word1.charAt(i));
+//             sb.append(word2.charAt(i));
+//             i++;
+//         }
+//         if(i!=word1.length()){            
+//             while (i<word1.length()){
+//                 sb.append(word1.charAt(i));
+//                 i++;
+//             }
+//         }
+//         else if(i!=word2.length()){            
+//             while (i<word2.length()){
+//                 sb.append(word2.charAt(i));
+//                 i++;
+//             }
+//         }
+//         return sb.toString();
+//     }
+// }
