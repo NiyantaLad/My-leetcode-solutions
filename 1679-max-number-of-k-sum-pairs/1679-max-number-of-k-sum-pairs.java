5b14376a212dc3a5238f7d2191aebffc22e60629
Niyanta Lad <niyantalad0604@gmail.com>
2023-06-26 16:38:20 -0400

    Time: 19 ms (100.00%), Space: 55.7 MB (36.11%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,29 +1,21 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-//         Map<Integer, Integer> map = new HashMap<>();
-//         int ct=0;
         
-//         for(int i=0;i<nums.length;i++){
-//             int diff=k-nums[i];            
-//             if(map.getOrDefault(diff,0)>0){
-//                 map.put(diff,map.get(diff)-1);
-//                 ct++;
-//             }
-//             else{
-//                 map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-//             }  
-//         }
-//         return ct;
-//     }
+        // Map<Integer, Integer> map = new HashMap<>();
+        // int result = 0;
+        // for(int i: nums)
+        //     if(map.containsKey(i) && map.get(i) > 0) {
+        //         map.put(i, map.get(i) - 1);
+        //         result ++;
+        //     } else map.put(k-i, map.getOrDefault(k-i, 0) + 1);
+        // return result;
         
-       
-	Map<Integer, Integer> map = new HashMap<>();
-	int result = 0;
-	for(int i: nums)
-		if(map.containsKey(i) && map.get(i) > 0) {
-			map.put(i, map.get(i) - 1);
-			result ++;
-		} else map.put(k-i, map.getOrDefault(k-i, 0) + 1);
-	return result;
-}
+        Arrays.sort(nums);
+        int result = 0, i =0, j= nums.length -1;
+        while(i<j)
+            if(nums[i]+nums[j] > k) j--;
+            else if(nums[i]+nums[j] < k) i++;
+            else {i++;j--;result++;}
+        return result;
+    }
 }
