c3e6553c46dd19ae9288f9c910898dd7e23aaf54
Niyanta Lad <niyantalad0604@gmail.com>
2023-06-28 14:56:59 -0400

    Time: 70 ms (48.90%), Space: 45.1 MB (12.81%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -6,12 +6,12 @@ class Solution {
         
         Map<Character, Integer> map1 = new HashMap<>();
         Map<Character, Integer> map2 = new HashMap<>();
-        
-        for(int i=0;i<A.length();i++){
-            map1.put(A.charAt(i), map1.getOrDefault(A.charAt(i),0)+1);
+        for(char i: A.toCharArray()){
+            map1.put(i,map1.getOrDefault(i,0)+1);
         }
-        for(int i=0;i<B.length();i++){
-            map2.put(B.charAt(i), map2.getOrDefault(B.charAt(i),0)+1);
+        
+        for(char i: B.toCharArray()){
+            map2.put(i,map2.getOrDefault(i,0)+1);
         }
         
         if(!map1.keySet().equals(map2.keySet()))
