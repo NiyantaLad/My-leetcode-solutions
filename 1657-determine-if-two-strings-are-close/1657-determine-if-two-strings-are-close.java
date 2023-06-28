1296fc3ae05dbe6adc15b568845401a9f2d7bf8b
Niyanta Lad <niyantalad0604@gmail.com>
2023-06-28 14:52:24 -0400

    Time: 72 ms (48.81%), Space: 45 MB (12.81%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean closeStrings(String A, String B) {
+        if (A.length()!=B.length() ) return false;
+        
+        if(A.equals(B))  {
+            return true;
+        }
+        
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+        
+        for(char i: A.toCharArray()){
+            map1.put(i,map1.getOrDefault(i,0)+1);
+        }
+        
+        for(char i: B.toCharArray()){
+            map2.put(i,map2.getOrDefault(i,0)+1);
+        }
+        if(!map1.keySet().equals(map2.keySet()))
+            return false;
+        
+        ArrayList<Integer> list1= new ArrayList<>(map1.values());
+        ArrayList<Integer> list2= new ArrayList<>(map2.values());
+        
+        
+        Collections.sort(list1);
+        Collections.sort(list2);
+        
+        return (list1.equals(list2));
+
+    }
+}
