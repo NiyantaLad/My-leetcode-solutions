d07c1930b3fc623e9192bde8f79a78a1410b77e5
Niyanta Lad <niyantalad0604@gmail.com>
2023-06-28 15:03:59 -0400

    Time: 13 ms (91.07%), Space: 44.7 MB (56.06%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -5,12 +5,17 @@ class Solution {
         
         int arr1[] = new int[26];
         int arr2[] = new int[26];
+        for(char c : word1.toCharArray())
+            arr1[c-'a']++;
+       
+        for(char c: word2.toCharArray())
+            arr2[c-'a']++;
+      
+//         for(int i=0;i<word1.length();i++)
+//             arr1[word1.charAt(i)-'a']++;
         
-        for(int i=0;i<word1.length();i++)
-            arr1[word1.charAt(i)-'a']++;
-        
-        for(int i=0;i<word2.length();i++)
-            arr2[word2.charAt(i)-'a']++;
+//         for(int i=0;i<word2.length();i++)
+//             arr2[word2.charAt(i)-'a']++;
         
         for(int i=0;i<26;i++)
             if((arr1[i]==0 && arr2[i]!=0) || (arr1[i]!=0 && arr2[i]==0))
