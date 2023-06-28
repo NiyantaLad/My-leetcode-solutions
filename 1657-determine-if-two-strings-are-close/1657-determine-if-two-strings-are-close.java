3b63dfa0c483e907ab27aea453932d42000c979f
Niyanta Lad <niyantalad0604@gmail.com>
2023-06-28 15:01:16 -0400

    Time: 17 ms (72.40%), Space: 44.4 MB (79.13%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,29 +1,57 @@
 class Solution {
-    public boolean closeStrings(String A, String B) {
-        if (A.length()!=B.length() ) return false;
-       
-        if(A.equals(B)) return true;
-        
-        Map<Character, Integer> map1 = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
-        for(char i: A.toCharArray()){
-            map1.put(i,map1.getOrDefault(i,0)+1);
-        }
-        
-        for(char i: B.toCharArray()){
-            map2.put(i,map2.getOrDefault(i,0)+1);
-        }
-        
-        if(!map1.keySet().equals(map2.keySet()))
+    public boolean closeStrings(String word1, String word2) {
+        if(word1.length()!=word2.length())
             return false;
         
-        ArrayList<Integer> list1= new ArrayList<>(map1.values());
-        ArrayList<Integer> list2= new ArrayList<>(map2.values());
+        int arr1[] = new int[26];
+        int arr2[] = new int[26];
         
-        Collections.sort(list1);
-        Collections.sort(list2);
+        for(int i=0;i<word1.length();i++)
+            arr1[word1.charAt(i)-'a']++;
         
-        return (list1.equals(list2));
-
+        for(int i=0;i<word2.length();i++)
+            arr2[word2.charAt(i)-'a']++;
+        
+        for(int i=0;i<26;i++)
+            if((arr1[i]==0 && arr2[i]!=0) || (arr1[i]!=0 && arr2[i]==0))
+                return false;
+        
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        
+        for(int i=0;i<26;i++)
+            if(arr1[i]!=arr2[i])
+                return false;
+        
+        return true;
     }
 }
+// class Solution {
+//     public boolean closeStrings(String A, String B) {
+//         if (A.length()!=B.length() ) return false;
+       
+//         if(A.equals(B)) return true;
+        
+//         Map<Character, Integer> map1 = new HashMap<>();
+//         Map<Character, Integer> map2 = new HashMap<>();
+//         for(char i: A.toCharArray()){
+//             map1.put(i,map1.getOrDefault(i,0)+1);
+//         }
+        
+//         for(char i: B.toCharArray()){
+//             map2.put(i,map2.getOrDefault(i,0)+1);
+//         }
+        
+//         if(!map1.keySet().equals(map2.keySet()))
+//             return false;
+        
+//         ArrayList<Integer> list1= new ArrayList<>(map1.values());
+//         ArrayList<Integer> list2= new ArrayList<>(map2.values());
+        
+//         Collections.sort(list1);
+//         Collections.sort(list2);
+        
+//         return (list1.equals(list2));
+
+//     }
+// }
