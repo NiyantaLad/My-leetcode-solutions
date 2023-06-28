8ef7e0d2d03fb7107f03d099dcf2887c6ea02d00
Niyanta Lad <niyantalad0604@gmail.com>
2023-06-28 15:38:02 -0400

    Time: 0 ms (100.00%), Space: 40.4 MB (42.20%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,10 +1,26 @@
 class Solution {
-    public int largestAltitude(int[] gain) {
-        int maxAltitude=gain[0];
-        for(int i=1;i<gain.length;i++){
-            gain[i]=gain[i]+gain[i-1];
-            maxAltitude = Math.max(maxAltitude,gain[i]);
-        }
-        return Math.max(0,maxAltitude);
+  public int largestAltitude(int[] gain) {
+    int currentAltitude = 0;
+    // Highest altitude currently is 0.
+    int highestPoint = currentAltitude;
+
+    for (int altitudeGain : gain) {
+      // Adding the gain in altitude to the current altitude.
+      currentAltitude += altitudeGain;
+      // Update the highest altitude.
+      highestPoint = Math.max(highestPoint, currentAltitude);
     }
+
+    return highestPoint;
+  }
 }
+// class Solution {
+//     public int largestAltitude(int[] gain) {
+//         int maxAltitude=gain[0];
+//         for(int i=1;i<gain.length;i++){
+//             gain[i]=gain[i]+gain[i-1];
+//             maxAltitude = Math.max(maxAltitude,gain[i]);
+//         }
+//         return Math.max(0,maxAltitude);
+//     }
+// }
