cb4f05ed311c915addd1b8f49c67ea8f0f0636f8
Niyanta Lad <niyantalad0604@gmail.com>
2023-06-28 14:21:31 -0400

    Time: 1 ms (99.12%), Space: 41.4 MB (86.91%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.java
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -1,6 +1,7 @@
  class Solution {
      public boolean buddyStrings(String A, String B) {
-        if (A.length() != B.length()) return false;
+        if (A.length() != B.length()) 
+            return false;
         if (A.equals(B)) {
             Set<Character> set = new HashSet<Character>();
             for (int i = 0; i < A.length(); i++) {
@@ -10,11 +11,41 @@
             }
             return false;
         }
+         
+         int firstIndex = -1;
+        int secondIndex = -1;
 
-        List<Integer> dif = new ArrayList<>();
-        for (int i = 0; i < A.length(); ++i) 
-            if (A.charAt(i) != B.charAt(i)) 
-                dif.add(i);
-        return dif.size() == 2 && A.charAt(dif.get(0)) == B.charAt(dif.get(1)) && A.charAt(dif.get(1)) == B.charAt(dif.get(0));
+        for (int i = 0; i < A.length(); ++i) {
+            if (A.charAt(i) != B.charAt(i)) {
+                if (firstIndex == -1) {
+                    firstIndex = i;
+                } else if (secondIndex == -1) {
+                    secondIndex = i;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if (secondIndex == -1) {
+            return false;
+        }
+
+        return A.charAt(firstIndex) == B.charAt(secondIndex) && 
+               A.charAt(secondIndex) == B.charAt(firstIndex);
+     
+         
+
+
+        // List<Integer> dif = new ArrayList<>();
+        // for (int i = 0; i < A.length(); ++i) {
+        //     if (A.charAt(i) != B.charAt(i)) 
+        //         dif.add(i);
+        // }
+        //     if (A.charAt(i) != B.charAt(i)) 
+        //         dif.add(i);
+        // return dif.size() == 2 
+        //     && A.charAt(dif.get(0)) == B.charAt(dif.get(1)) 
+        //     && A.charAt(dif.get(1)) == B.charAt(dif.get(0));
     }
  }
