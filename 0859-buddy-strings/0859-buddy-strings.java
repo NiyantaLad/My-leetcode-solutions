466c514625128c59e75b92262ed44b0502a4e689
Niyanta Lad <niyantalad0604@gmail.com>
2023-06-28 14:07:26 -0400

    Time: 4 ms (50.44%), Space: 41.5 MB (86.91%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.java
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -1,18 +1,16 @@
-class Solution {
-    public boolean buddyStrings(String A, String B) {
-    if (A == null || B == null || A.length() != B.length())  return false;
-    int a = -1, b = -1, diff = 0;
-    int[] count = new int[26];
-    // check if able to switch with the same character.
-    boolean canSwitch = false;
-    for (int i = 0; i < A.length(); i++) {
-      if (++count[A.charAt(i) - 'a'] >= 2)  canSwitch = true;
-      if (A.charAt(i) != B.charAt(i)) {
-        diff++;
-        if (a == -1)  a = i;
-        else if (b == -1)  b = i;
-      }
+ class Solution {
+     public boolean buddyStrings(String A, String B) {
+        if (A.length() != B.length()) return false;
+        if (A.equals(B)) {
+            Set<Character> s = new HashSet<Character>();
+            for (char c : A.toCharArray())
+                s.add(c);
+            return s.size() < A.length();
+        }
+        List<Integer> dif = new ArrayList<>();
+        for (int i = 0; i < A.length(); ++i) 
+            if (A.charAt(i) != B.charAt(i)) 
+                dif.add(i);
+        return dif.size() == 2 && A.charAt(dif.get(0)) == B.charAt(dif.get(1)) && A.charAt(dif.get(1)) == B.charAt(dif.get(0));
     }
-    return (diff == 0 && canSwitch) || (diff == 2 && A.charAt(a) == B.charAt(b) && A.charAt(b) == B.charAt(a));
-  }
-}
+ }
