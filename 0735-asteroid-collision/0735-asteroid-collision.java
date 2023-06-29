9a862e57b4e72b38c94c1645fd04e76e69073ef8
Niyanta Lad <niyantalad0604@gmail.com>
2023-06-29 18:36:28 -0400

    Time: 12 ms (54.13%), Space: 44.3 MB (32.82%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,41 +1,71 @@
 class Solution {
-    public int[] asteroidCollision(int[] asteroids) {
+        public int[] asteroidCollision(int[] asteroids) {
+        if (asteroids.length <= 1) return asteroids;  // Handling Corner cases
         
         Stack<Integer> stack = new Stack<>();
-        
-        for (int asteroid : asteroids) {
-            boolean flag = true;
-            while (!stack.isEmpty() && (stack.peek() > 0 && asteroid < 0)) {
-                // If the top asteroid in the stack is smaller, then it will explode.
-                // Hence pop it from the stack, also continue with the next asteroid in the stack.
-                if (Math.abs(stack.peek()) < Math.abs(asteroid)) {
+        for (int asteriod : asteroids) {
+            if (asteriod > 0) { // Pushing all +ve asteroids
+                stack.push(asteriod);
+            } else {
+                // Remove all positive asteroids before our current asteroid
+                while (!stack.isEmpty() && stack.peek() > 0 && Math.abs(stack.peek()) < Math.abs(asteriod))  
                     stack.pop();
-                    continue;
-                }
-                // If both asteroids have the same size, then both asteroids will explode.
-                // Pop the asteroid from the stack; also, we won't push the current asteroid to the stack.
-                else if (Math.abs(stack.peek()) == Math.abs(asteroid)) {
+                // Checking if the stack is empty or the recent asteriod is negative!
+                if (stack.isEmpty() || stack.peek() < 0) 
+                    stack.push(asteriod);
+                // If recent asteriod <= our asteriod, We broke our outer loop if equal we pop it.
+                else if (stack.peek() == Math.abs(asteriod)) 
                     stack.pop();
-                }
-
-                // If we reach here, the current asteroid will be destroyed
-                // Hence, we should not add it to the stack
-                flag = false;
-                break;
             }
-
-            if (flag) {
-                stack.push(asteroid);
-            }
-            
         }
         
-        int[] result = new int[stack.size()];
-        for(int i=result.length-1;i>=0;i--){
-            result[i]=stack.pop();
-        }
+        int[] output = new int[stack.size()];
+        for (int i = output.length - 1; i >= 0; i--)
+            output[i] = stack.pop();
         
-        return result;
-        
+        return output;
     }
+
 }
+
+// class Solution {
+//     public int[] asteroidCollision(int[] asteroids) {
+        
+//         Stack<Integer> stack = new Stack<>();
+        
+//         for (int asteroid : asteroids) {
+//             boolean flag = true;
+//             while (!stack.isEmpty() && (stack.peek() > 0 && asteroid < 0)) {
+//                 // If the top asteroid in the stack is smaller, then it will explode.
+//                 // Hence pop it from the stack, also continue with the next asteroid in the stack.
+//                 if (Math.abs(stack.peek()) < Math.abs(asteroid)) {
+//                     stack.pop();
+//                     continue;
+//                 }
+//                 // If both asteroids have the same size, then both asteroids will explode.
+//                 // Pop the asteroid from the stack; also, we won't push the current asteroid to the stack.
+//                 else if (Math.abs(stack.peek()) == Math.abs(asteroid)) {
+//                     stack.pop();
+//                 }
+
+//                 // If we reach here, the current asteroid will be destroyed
+//                 // Hence, we should not add it to the stack
+//                 flag = false;
+//                 break;
+//             }
+
+//             if (flag) {
+//                 stack.push(asteroid);
+//             }
+            
+//         }
+        
+//         int[] result = new int[stack.size()];
+//         for(int i=result.length-1;i>=0;i--){
+//             result[i]=stack.pop();
+//         }
+        
+//         return result;
+        
+//     }
+// }
