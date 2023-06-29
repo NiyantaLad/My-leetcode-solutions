6843cb68af7e445fb6884b42e8ceefe531f99d4b
Niyanta Lad <niyantalad0604@gmail.com>
2023-06-29 18:32:24 -0400

    Time: 16 ms (10.58%), Space: 44.7 MB (16.33%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        int max=0;
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int asteroid : asteroids) {
+            boolean flag = true;
+            while (!stack.isEmpty() && (stack.peek() > 0 && asteroid < 0)) {
+                // If the top asteroid in the stack is smaller, then it will explode.
+                // Hence pop it from the stack, also continue with the next asteroid in the stack.
+                if (Math.abs(stack.peek()) < Math.abs(asteroid)) {
+                    stack.pop();
+                    continue;
+                }
+                // If both asteroids have the same size, then both asteroids will explode.
+                // Pop the asteroid from the stack; also, we won't push the current asteroid to the stack.
+                else if (Math.abs(stack.peek()) == Math.abs(asteroid)) {
+                    stack.pop();
+                }
+
+                // If we reach here, the current asteroid will be destroyed
+                // Hence, we should not add it to the stack
+                flag = false;
+                break;
+            }
+
+            if (flag) {
+                stack.push(asteroid);
+            }
+            
+        }
+        
+        System.out.println(stack);
+        int[] result = new int[stack.size()];
+        for(int i=result.length-1;i>=0;i--){
+            result[i]=stack.pop();
+        }
+        
+        return result;
+        
+    }
+}
