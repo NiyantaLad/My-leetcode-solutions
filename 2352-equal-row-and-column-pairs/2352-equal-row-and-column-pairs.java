3d3086525dde0687c2dd40246e49d88e920256b3
Niyanta Lad <niyantalad0604@gmail.com>
2023-06-28 21:31:03 -0400

    Time: 29 ms (87.42%), Space: 55.7 MB (5.37%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,24 +1,61 @@
+class TrieNode {
+    int count;
+    Map<Integer, TrieNode> children;
+
+    public TrieNode() {
+        this.count = 0;
+        this.children = new HashMap<>();
+    }
+}
+
+class Trie {
+    TrieNode trie;
+
+    public Trie() {
+        this.trie = new TrieNode();
+    }
+
+    public void insert(int[] array) {
+        TrieNode myTrie = this.trie;
+        for (int a : array) {
+            if (!myTrie.children.containsKey(a)) {
+                myTrie.children.put(a, new TrieNode());
+            }
+            myTrie = myTrie.children.get(a);
+        }
+        myTrie.count += 1;
+    }
+
+    public int search(int[] array) {
+        TrieNode myTrie = this.trie;
+        for (int a : array) {
+            if (myTrie.children.containsKey(a)) {
+                myTrie = myTrie.children.get(a);
+            } else {
+                return 0;
+            }
+        }
+        return myTrie.count;
+    }
+}
+
 class Solution {
     public int equalPairs(int[][] grid) {
-        int count = 0;
-        int n = grid.length;
-
-        // Keep track of the frequency of each row.
-        Map<String, Integer> rowCounter = new HashMap<>();
+        Trie myTrie = new Trie();
+        int count = 0, n = grid.length;
+        
         for (int[] row : grid) {
-            String rowString = Arrays.toString(row);
-            rowCounter.put(rowString, 1 + rowCounter.getOrDefault(rowString, 0));
+            myTrie.insert(row);
         }
-
-        // Add up the frequency of each column in map.
-        for (int c = 0; c < n; c++) {
+        
+        for (int c = 0; c < n; ++c) {
             int[] colArray = new int[n];
             for (int r = 0; r < n; ++r) {
                 colArray[r] = grid[r][c];
             }
-            count += rowCounter.getOrDefault(Arrays.toString(colArray), 0);
+            count += myTrie.search(colArray);
         }
-
+    
         return count;
     }
 }
