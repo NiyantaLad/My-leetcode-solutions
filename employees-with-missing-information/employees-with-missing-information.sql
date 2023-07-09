32dc55d885b151c99ab3bfd682b649284bdd98e7
Niyanta Lad <niyantalad0604@gmail.com>
2023-07-09 19:25:18 -0400

    Time: 1079 ms, Memory: 0B - LeetHub

---
+++ employees-with-missing-information/employees-with-missing-information.sql
@@ -0,0 +1,19 @@
+select employee_id from employees where employee_id not in
+(select employee_id from salaries)
+
+union
+
+select employee_id from salaries where employee_id not in
+(select employee_id from employees)
+
+order by employee_id asc
+
+
+# SELECT T.employee_id
+# FROM  
+#   (SELECT * FROM Employees LEFT JOIN Salaries USING(employee_id)
+#    UNION 
+#    SELECT * FROM Employees RIGHT JOIN Salaries USING(employee_id))
+# AS T
+# WHERE T.salary IS NULL OR T.name IS NULL
+# ORDER BY employee_id;
