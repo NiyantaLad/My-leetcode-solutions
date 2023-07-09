c257b62a7dcded289fd80ae515e351d161d5f807
Niyanta Lad <niyantalad0604@gmail.com>
2023-07-09 16:29:23 -0400

    Time: 3188 ms, Memory: 0B - LeetHub

---
+++ market-analysis-i/market-analysis-i.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select user_id as buyer_id, join_date, count(order_id) as orders_in_2019 
+from users left join orders 
+on users.user_id=orders.buyer_id and year(order_date)='2019'
+group by user_id
